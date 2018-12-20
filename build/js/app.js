7ee252783f7623d58532d6d66f73c1acc460ee03
Lyapunov Mihail <LyapunovMihail@gmail.com>
2018-12-20 23:02:55 +0400

    layout_finish

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,57 @@
-"use strict";function closeModal(){let e=document.querySelector(".modal-visible");e&&(e.classList.remove("modal-visible"),this.removeEventListener("click",closeModal))}var yandexMap;function init(){yandexMap=new ymaps.Map("ya-map",{center:[59.938631,30.323055],zoom:15,controls:["zoomControl"]},{searchControlProvider:"yandex#search"});let e=new ymaps.Placemark([59.938631,30.323055],{hintContent:"ул. Б. Конюшенная, д. 19/8"},{iconLayout:"default#image",iconImageHref:"img/bg/map-marker-png.png",iconImageSize:[231,190],iconImageOffset:[-58,-193]});yandexMap.geoObjects.add(e),yandexMap.behaviors.disable("scrollZoom")}document.ready=function(){let e=document.querySelector(".js-modal-target");e&&e.addEventListener("click",function(e){e&&e.preventDefault();let a=document.querySelector(".js-modal");a&&(a.classList.add("modal-visible"),a.querySelector(".js-modal-close").addEventListener("click",closeModal))})}(),ymaps.ready(init);
+'use strict';
+
+document.ready = (function() {
+    // Вешаем обработчик на открытие модального окна
+    let modalBtn = document.querySelector('.js-modal-target');
+    if (modalBtn) {
+        modalBtn.addEventListener('click', function(evt) {
+            if (evt) {
+                evt.preventDefault();
+            }
+            let modal = document.querySelector(`.js-modal`);
+            if (modal) {
+                modal.classList.add('modal-visible');
+                
+                // Вешаем обработчик закрытия модального окна
+                modal.querySelector('.js-modal-close').addEventListener('click', closeModal);
+            }
+        });       
+    }
+})();
+
+// Закрытия модального окна
+function closeModal() {
+    let modal = document.querySelector('.modal-visible');
+    if (modal) {
+        modal.classList.remove('modal-visible');
+        this.removeEventListener('click', closeModal);
+    }
+}
+
+
+var yandexMap;
+// Дождёмся загрузки API и готовности DOM.
+ymaps.ready(init);
+
+function init () {
+    // Создание экземпляра карты и его привязка к контейнеру с
+    // заданным id ("map").
+    yandexMap = new ymaps.Map('ya-map', {
+        center: [59.938631, 30.323055],
+        zoom: 15,
+        controls: ["zoomControl"]
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+    let myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
+        hintContent: 'ул. Б. Конюшенная, д. 19/8'
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/bg/map-marker-png.png',
+        iconImageSize: [231, 190],
+        iconImageOffset: [-58, -193]
+    });
+    yandexMap.geoObjects.add(myPlacemark);
+    yandexMap.behaviors.disable("scrollZoom");
+}
+//# sourceMappingURL=app.js.map
